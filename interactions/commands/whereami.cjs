aeb547934ae6bc21db62900b5e21a261a0e9c995
ImmortalDonkey <donkeyimmortal@gmail.com>
2025-11-26 16:36:40 +0000

    Update whereami.cjs

--- interactions/commands/whereami.cjs
+++ interactions/commands/whereami.cjs
@@ -1,1 +1,32 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "whereami",
+
+  async execute(client, interaction) {
+    const user = interaction.user;
+    const playerLocations = client.playerLocations;
+
+    const data = playerLocations.get(user.id);
+
+    if (!data) {
+      return interaction.reply({
+        content: "❌ You haven't set a location!",
+        ephemeral: true
+      });
+    }
+
+    return interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Blue")
+          .setTitle("📍 Your Location")
+          .addFields(
+            { name: "Location", value: data.location },
+            { name: "Updated", value: data.timestamp.toLocaleString() }
+          )
+      ]
+    });
+  }
+};
 
