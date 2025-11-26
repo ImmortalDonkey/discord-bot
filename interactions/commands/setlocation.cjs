fd6f73f820a055c679eefc1af31bda94961f2443
ImmortalDonkey <donkeyimmortal@gmail.com>
2025-11-26 16:36:29 +0000

    Update setlocation.cjs

--- interactions/commands/setlocation.cjs
+++ interactions/commands/setlocation.cjs
@@ -1,1 +1,29 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "setlocation",
+
+  async execute(client, interaction) {
+    const loc = interaction.options.getString("location");
+    const user = interaction.user;
+
+    // Shared global or client-attached map
+    const playerLocations = client.playerLocations;
+
+    playerLocations.set(user.id, {
+      location: loc,
+      timestamp: new Date(),
+      username: user.username
+    });
+
+    return interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor("Green")
+          .setTitle("📍 Location Updated")
+          .setDescription(`Your location is now **${loc}**`)
+      ]
+    });
+  }
+};
 
