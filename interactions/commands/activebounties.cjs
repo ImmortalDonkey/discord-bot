87f126745b8da24586dd10e89641bba0854c8f24
ImmortalDonkey <donkeyimmortal@gmail.com>
2025-11-26 21:07:07 +0000

    Update activebounties.cjs

--- interactions/commands/activebounties.cjs
+++ interactions/commands/activebounties.cjs
@@ -1,1 +1,12 @@
+// interactions/commands/activebounties.cjs
+module.exports = {
+  name: "activebounties",
+
+  async execute(client, interaction) {
+    return interaction.reply({
+      content: "📝 Active bounties will be added soon.",
+      ephemeral: true
+    });
+  }
+};
 
