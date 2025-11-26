faf35ef37289832e73880f41ea6449082a1f49b8
ImmortalDonkey <donkeyimmortal@gmail.com>
2025-11-26 17:37:02 +0000

    Create locations.cjs

---
+++ utils/locations.cjs
@@ -0,0 +1,12 @@
+// utils/locations.cjs
+
+const availableLocations = [
+  "Route 1", "Route 2", "Route 3", "Route 4", "Route 6", "Route 7",
+  "Route 8", "Route 9", "Route 10", "Route 11", "Route 12", "Route 13",
+  "Route 14", "Route 15", "Route 16", "Route 17", "Route 18", "Route 19",
+  "Route 20", "Route 21", "Route 22", "Route 23", "Route 24", "Route 25",
+  "Mudbray Ranch", "New Haven", "Nightshade", "Shore's End",
+  "Stillwater Quarry", "Wild Overgrowth"
+];
+
+module.exports = { availableLocations };
