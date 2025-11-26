e1f48887ff8936f1a3df1d5ed45791ff808f3367
ImmortalDonkey <donkeyimmortal@gmail.com>
2025-11-26 16:37:18 +0000

    Update clearme.cjs

--- interactions/commands/clearme.cjs
+++ interactions/commands/clearme.cjs
@@ -1,1 +1,18 @@
+// interactions/commands/clearme.cjs
+
+module.exports = {
+  name: "clearme",
+
+  async execute(client, interaction) {
+    const user = interaction.user;
+    const playerLocations = client.playerLocations;
+
+    playerLocations.delete(user.id);
+
+    return interaction.reply({
+      content: "🧹 You were marked inactive.",
+      ephemeral: true
+    });
+  }
+};
 
