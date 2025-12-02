e91b33c0c436f8e0e6c42853a55017ef671b43a2
ImmortalDonkey <donkeyimmortal@gmail.com>
2025-12-02 16:15:11 +0000

    Create locationDebugAuto.cjs

---
+++ interactions/autocomplete/locationDebugAuto.cjs
@@ -0,0 +1,29 @@
+// interactions/autocomplete/locationDebugAuto.cjs
+
+const availableLocations = [
+  "Route 1","Route 2","Route 3","Route 4","Route 6","Route 7",
+  "Route 8","Route 9","Route 10","Route 11","Route 12","Route 13",
+  "Route 14","Route 15","Route 16","Route 17","Route 18","Route 19",
+  "Route 20","Route 21","Route 22","Route 23","Route 24","Route 25",
+  "Mudbray Ranch","New Haven","Nightshade",
+  "Shore's End","Stillwater Quarry","Wild Overgrowth"
+];
+
+function filterLocations(interaction) {
+  const focused = interaction.options.getFocused() || "";
+  return availableLocations
+    .filter(loc => loc.toLowerCase().includes(focused.toLowerCase()))
+    .slice(0, 25)
+    .map(l => ({ name: l, value: l }));
+}
+
+module.exports = [
+  {
+    commandName: "reportdebug",
+    optionName: "route",
+
+    async execute(interaction) {
+      return interaction.respond(filterLocations(interaction));
+    }
+  }
+];
